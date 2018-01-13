f5bfc54ceb8654a6106785654797e3a12c88d0d2
ismoon <insu4341@gmail.com>
2018-01-13 17:49:54 +0900

    first commit

---
+++ src/com/ismoon/design_pattern/decorator/Application.java
@@ -0,0 +1,11 @@
+package com.ismoon.design_pattern.decorator;
+
+/**
+ * @author ismoon
+ * @since 2017-11-16.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
